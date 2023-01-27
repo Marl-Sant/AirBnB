e63058f75dbe0d5a8b741000910b17e753ee89cd
Marlon Santos <santosfmarlon@gmail.com>
2023-01-26 20:49:07 -0500

    fixed the downs

--- backend/db/migrations/20230119061137-create-review.js
+++ backend/db/migrations/20230119061137-create-review.js
@@ -56,6 +56,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = "Reviews";
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20230119220135-create-review-image.js
+++ backend/db/migrations/20230119220135-create-review-image.js
@@ -41,6 +41,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    options.tableName = "ReviewImages";
+    await queryInterface.dropTable(options);
   }
 };
