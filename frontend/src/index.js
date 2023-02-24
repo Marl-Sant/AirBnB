084a618ae212822ce9b65c9a251d3b6fdc0be562
Marlon Santos <santosfmarlon@gmail.com>
2023-02-24 13:07:10 -0500

    wrapped-fetch-with-CSRF

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
